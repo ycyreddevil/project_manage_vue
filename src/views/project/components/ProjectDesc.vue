f8c05a740a270a58efdda87b9adf6f6315236930
ycyreddevil <Ycy910663>
2020-07-21 17:16:58 +0800

    项目详情静态页面以及对应组件调整优化

---
+++ src/views/project/components/ProjectDesc.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <el-row type="flex" justify="space-between">
+      <el-col :span="16">
+        <el-row type="flex" justify="space-around">
+          <el-col :span="11" style="overflow-y: auto">
+            <div class="chart-wrapper">
+              <bar-chart />
+            </div>
+          </el-col>
+          <el-col :span="11" style="overflow-y: auto">
+            <div class="chart-wrapper">
+              <project-dynamic-simple />
+            </div>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="space-around">
+          <el-col :span="11" style="overflow-y: auto">
+            <div class="chart-wrapper">
+              <project-member-simple />
+            </div>
+          </el-col>
+          <el-col :span="11" style="overflow-y: auto">
+            <div class="chart-wrapper">
+              <project-history-simple />
+            </div>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="8">
+        <detail-content />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import ProjectHistorySimple from '@/views/project/components/ProjectHistorySimple'
+import ProjectMemberSimple from '@/views/project/components/ProjectMemberSimple'
+import ProjectDynamicSimple from '@/views/project/components/ProjectDynamicSimple'
+import BarChart from '@/views/dashboard/admin/components/BarChart'
+import DetailContent from '@/views/project/components/DetailContent'
+
+export default {
+  name: 'ProjectDesc',
+  components: { ProjectHistorySimple, ProjectMemberSimple, ProjectDynamicSimple, BarChart, DetailContent }
+}
+</script>
+<style lang="scss" scoped>
+  .chart-wrapper {
+    height: 28rem;
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+</style>
--- src/views/project/detail.vue
+++ src/views/project/detail.vue
@@ -1,52 +1,40 @@
 <template>
   <div class="dashboard-editor-container">
-    <el-row type="flex" justify="space-between">
-      <el-col :span="16">
-        <el-row type="flex" justify="space-around">
-          <el-col :span="11" style="overflow-y: auto">
-            <div class="chart-wrapper">
-              <bar-chart />
-            </div>
-          </el-col>
-          <el-col :span="11" style="overflow-y: auto">
-            <div class="chart-wrapper">
-              <project-dynamic />
-            </div>
-          </el-col>
-        </el-row>
-        <el-row type="flex" justify="space-around">
-          <el-col :span="11" style="overflow-y: auto">
-            <div class="chart-wrapper">
-              <project-member />
-            </div>
-          </el-col>
-          <el-col :span="11" style="overflow-y: auto">
-            <div class="chart-wrapper">
-              <project-history />
-            </div>
-          </el-col>
-        </el-row>
-      </el-col>
-      <el-col :span="8">
-        <detail-content />
-      </el-col>
-    </el-row>
+    <div>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="项目概述" name="desc"><project-desc /></el-tab-pane>
+        <el-tab-pane label="团队管理" name="team" />
+        <el-tab-pane label="项目任务" name="task" />
+        <el-tab-pane label="项目动态" name="dynamic" />‘
+        <el-tab-pane label="预算管理" name="budget" />
+      </el-tabs>
+    </div>
   </div>
 </template>
 <script>
-import BarChart from '@/views/dashboard/admin/components/BarChart'
-import ProjectDynamic from '@/views/project/components/ProjectDynamic'
-import DetailContent from '@/views/project/components/DetailContent'
-import ProjectMember from '@/views/project/components/ProjectMember'
-import ProjectHistory from '@/views/project/components/ProjectHistory'
+import ProjectDesc from '@/views/project/components/ProjectDesc'
 export default {
   name: 'ProjectDetail',
-  components: { ProjectHistory, ProjectMember, ProjectDynamic, BarChart, DetailContent }
+  components: { ProjectDesc },
+  data() {
+    return {
+      activeName: 'desc'
+    }
+  },
+  watch: {
+    activeName(val) {
+    }
+  },
+  methods: {
+    showCreatedTimes() {
+
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
   .dashboard-editor-container {
-    padding: 32px;
+    padding: 20px;
     background-color: rgb(240, 242, 245);
     position: relative;
     .chart-wrapper {
